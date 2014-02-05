8fe925a48384248bab17f8a2a40b766e9f74c21d
Mihail Zdravkov <mihail0zdravkov@gmail.com>
2014-02-05 02:16:26 +0200

    Make Beyond::ApplicationController inherit from the main app. By doing this, the engine will now share layouts and sessions from the main app.

--- app/controllers/beyond/application_controller.rb
+++ app/controllers/beyond/application_controller.rb
@@ -1,4 +1,4 @@
-module Beyond
-  class ApplicationController < ActionController::Base
+#module Beyond
+  class Beyond::ApplicationController < ApplicationController #ActionController::Base <- removed in order to inherit from host app's layout and session
   end
-end
+#end
